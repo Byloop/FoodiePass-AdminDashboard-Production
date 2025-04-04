00e8dfbf2eaa7c97ff524b8cb334856712e1bf86
byloop <test.byloop@gmail.com>
2025-04-04 12:26:08 +0530

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import { BrowserRouter } from 'react-router-dom';
-import Router from './routes/AdminRouter';
-import 'swiper/css';
-import i18n from './i18n';
-import { I18nextProvider } from 'react-i18next';
-import { Provider } from '@/components/ui/provider';
+
+
 
 function App() {
   return (
     <I18nextProvider i18n={i18n}>
-      <Provider>
-        <BrowserRouter>
-          <Router />
-        </BrowserRouter>
-      </Provider>
+      <div>Testing</div>
     </I18nextProvider>
   );
 }
--- src/pages/plan-management/index.jsx
+++
@@ -1,5 +0,0 @@
-function PlanManagement() {
-  return <div className="w-full pt-6 px-4 xlg:px-6"></div>;
-}
-
-export default PlanManagement;
